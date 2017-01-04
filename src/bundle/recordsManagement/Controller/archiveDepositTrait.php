35da23731be6a1ea8094b6948ff78c59fbb96776
Alexis Ragot <alexis.ragot@maarch.org>
2017-01-04 17:30:31 +0100

    utilisation of bundle documentManagement was removed

--- src/bundle/recordsManagement/Controller/archiveDepositTrait.php
+++ src/bundle/recordsManagement/Controller/archiveDepositTrait.php
@@ -140,7 +140,7 @@ trait archiveDepositTrait
 
             $this->validateArchiveDescriptionObject($archive);
         }
-        // Documents
+        // Resources
         if ($archive->digitalResources) {
             //$formatController = \laabs::newController("digitalResource/format");
             $droid = \laabs::newService('dependency/fileSystem/plugins/fid');
@@ -148,7 +148,7 @@ trait archiveDepositTrait
 
             foreach ($archive->digitalResources as $digitalResource) {
                 if (empty($digitalResource->archiveId)) {
-                    $digitalResource->archiveId = $archive->archiveId;       
+                    $digitalResource->archiveId = $archive->archiveId;
                 }
                 if (empty($digitalResource->resId)) {
                     $digitalResource->resId = \laabs::newId();
