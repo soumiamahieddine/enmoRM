a5b4a219af8a583c4bc98adb60445a8a2eae228d
Prosper De Laure <prosper.delaure@maarch.org>
2017-03-21 15:03:12 +0100

    sample data modification

--- data/maarchRM/sql/pgsql/filePlan.sql
+++ data/maarchRM/sql/pgsql/filePlan.sql
@@ -5,5 +5,8 @@ DELETE FROM "filePlan"."position";
 INSERT INTO "filePlan"."folder"
 ("folderId", "name", "parentFolderId", "description", "ownerOrgRegNumber") VALUES 
 ('1','Facture client',null,'Facture client','regNum_CPTCLI'),
-('2','Maarch les bains','1','Ville de Maarch les bains','regNum_CPTCLI'),
-('3','ACME','1','Societe ACME','regNum_CPTCLI');
+('2','Thomson-CSF','1','Facture client','regNum_CPTCLI'),
+('3','Hummingbird','1','Facture client','regNum_CPTCLI'),
+('4','Adobe','1','Facture client','regNum_CPTCLI'),
+('5','Maarch les bains','1','Ville de Maarch les bains','regNum_CPTCLI'),
+('6','ACME','1','Societe ACME','regNum_CPTCLI');
