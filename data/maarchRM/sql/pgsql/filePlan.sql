463ac6d5d8b6b790171ff5edae15427277fbc529
Prosper De Laure <prosper.delaure@maarch.org>
2017-03-22 17:18:27 +0100

    New demo data

--- data/maarchRM/sql/pgsql/filePlan.sql
+++ data/maarchRM/sql/pgsql/filePlan.sql
@@ -4,9 +4,6 @@ DELETE FROM "filePlan"."position";
 -- communication --
 INSERT INTO "filePlan"."folder"
 ("folderId", "name", "parentFolderId", "description", "ownerOrgRegNumber") VALUES 
-('1','Facture client',null,'Facture client','regNum_CPTCLI'),
-('2','Thomson-CSF','1','Facture client','regNum_CPTCLI'),
-('3','Hummingbird','1','Facture client','regNum_CPTCLI'),
-('4','Adobe','1','Facture client','regNum_CPTCLI'),
-('5','Maarch les bains','1','Ville de Maarch les bains','regNum_CPTCLI'),
-('6','ACME','1','Societe ACME','regNum_CPTCLI');
+('1','Adobe','','Facture client d''Adobe','regNum_CPTCLI'),
+('2','Eurotunnel','','Facture client d''Eurotunnel','regNum_CPTCLI'),
+('3','Eurosam','','Facture client d''Eurosam','regNum_CPTCLI');
