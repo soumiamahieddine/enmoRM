ebebcbc09614f9e1448a48235d1e83d863690d47
Alexis Ragot <alexis.ragot@maarch.org>
2017-06-28 15:35:24 +0200

    fix archiveUnit name

--- src/bundle/recordsManagement/Controller/archiveEntryTrait.php
+++ src/bundle/recordsManagement/Controller/archiveEntryTrait.php
@@ -139,6 +139,7 @@ trait archiveEntryTrait
                 $archiveUnit = $this->extractArchiveUnit($filename);
                 $archiveUnit->archiveId = \laabs::newId();
                 $archiveUnit->digitalResources[] = $this->extractResource($directory, $filename);
+                $archiveUnit->archiveName = basename($archiveUnit->archiveName, $archiveUnit->digitalResources[0]->fileExtension);
                 $archive->contents[] = $archiveUnit;
             }
         }
