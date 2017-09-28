a9be4f5802b0daeb9dffa4494cbfeef003e57125
Alexandre Morin <alexandre.morin@maarch.org>
2017-09-28 10:47:18 +0200

    Update data scheduling

--- data/maarchRM/sql/pgsql/default.sql
+++ data/maarchRM/sql/pgsql/default.sql
@@ -63,8 +63,8 @@ INSERT INTO "batchProcessing"."task"
 
 INSERT INTO "batchProcessing"."scheduling"
 ("schedulingId", "name", "executedBy", "taskId", "frequency","parameters","lastExecution","nextExecution","status") VALUES
-('chainJournalAudit', 'Chaînage audit', 'System', '01', '00;00;;;;;;;',null,null,null,'scheduled'),
-('chainJournalLifeCycle', 'Chaînage journaux', 'System', '02', '00;01;;;;;;;', null,null,null,'scheduled'),
-('integrity', 'Intégrité', 'System','03', '00;02;;;;;;;',null,null,null,'scheduled'),
-('deleteArchive', 'Destruction', 'System', '04', '00;03;;;;;;;', null,null,null,'scheduled'),
+('chainJournalAudit', 'Chaînage audit', 'System', '01', '00;00;;;;;;;',null,null,null,'paused'),
+('chainJournalLifeCycle', 'Chaînage journaux', 'System', '02', '00;01;;;;;;;', null,null,null,'paused'),
+('integrity', 'Intégrité', 'System','03', '00;02;;;;;;;',null,null,null,'paused'),
+('deleteArchive', 'Destruction', 'System', '04', '00;03;;;;;;;', null,null,null,'paused'),
 ('sendNotification', 'Envoie des notifications', 'System', '05', '00;04;;;;;;;', null,null,null,'paused');
