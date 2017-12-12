93665ee692de568ef48a84d38b7c4e80d45e3dc0
Correia Lopes Dylan <dylan.correia@hotmail.com>
2017-12-12 12:18:42 +0100

    fix translation 3

--- src/presentation/maarchRM/Presenter/recordsManagement/archive.php
+++ src/presentation/maarchRM/Presenter/recordsManagement/archive.php
@@ -298,9 +298,7 @@ class archive
         foreach ($archive->message as $message) {
             $message->type = $this->view->translator->getText($message->type, false, "recordsManagement/messages");
         }
-        $dataTable = $this->view->getElementsByClass("dataTable")->item(0)->plugin['dataTable'];
-        $dataTable->setSorting(array(array(2, 'asc')));
-
+      
         //$this->view->setSource("visible", $visible);
         $this->view->setSource("archive", $archive);
 
