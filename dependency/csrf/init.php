4d3d2817fc14a527270db8bb916b303dfe8e96cc
Alexis Ragot <alexis.ragot@maarch.org>
2018-02-13 10:59:56 +0100

    csrf dependency with observer

--- dependency/csrf/init.php
+++ dependency/csrf/init.php
@@ -1,34 +1,10 @@
 <?php 
 namespace dependency\csrf;
 
-$observer = new CsrfObserver();
+$observer = \laabs::newService("dependency/csrf/CsrfObserver");
 
 \core\Observer\Dispatcher::attach(
     $observer,
     'observeRequest',
     LAABS_REQUEST
 );
-
-class CsrfObserver
-{
-    /**
-     * Whitelist for post methods
-     * @var array
-     */
-    protected $whiteList = [];
-
-    /**
-     * Observer for user authentication
-     * @param \core\Reflection\Command &$servicePath
-     * @param array                    &$args
-     *
-     * @return auth/credential
-     *
-     * @subject LAABS_REQUEST
-     */
-    public function observeRequest(&$httpRequest, array &$args = null)
-    {
-        require_once "../dependency/csrf/libs/csrf/csrfprotector.php";
-        \csrfProtector::init();
-    }
-}
