ee90c9d4c2254593b6bcd2c93c108f4387a95ea0
Shiva SIVANESARAJAH <shiva.srajah@maarch.org>
2018-09-28 11:39:26 +0200

    Fix fileplanLevel for journal archive #2

--- src/bundle/recordsManagement/Controller/log.php
+++ src/bundle/recordsManagement/Controller/log.php
@@ -326,7 +326,7 @@ class log implements archiveDescriptionInterface
         $archive->accessRuleDuration = 'P0D';
         $archive->retentionDuration = 'P0D';
         $archive->finalDisposition = 'preservation';
-        $archive->filePlanLevel = 'item';
+        $archive->fileplanLevel = 'item';
         $archive->archiveName = 'journal/'.$log->type.' '.date_format($log->fromDate, 'Y/m/d').' - '.date_format($log->toDate, 'Y/m/d');
 
         if (!empty($this->logFilePlan)) {
