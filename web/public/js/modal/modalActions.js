ce5e8681204b40b59ab2c8c97ae95a861b0046a7
arnaud.pauget <arnaud.pauget@maarch.org>
2018-12-27 15:43:34 +0100

    camelCase on button class

--- web/public/js/modal/modalActions.js
+++ web/public/js/modal/modalActions.js
@@ -1,4 +1,4 @@
-$(".actionbutton").on('click', function () {
+$(".actionButton").on('click', function () {
         var archiveIds = [$(this).data("archiveid")];
         var actionId = $(this).data("actionid");
         WorkflowAction.send(actionId, archiveIds);
