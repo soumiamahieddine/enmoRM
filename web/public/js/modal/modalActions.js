c889148945ddd2f5fa2451235d86b082592b18a0
arnaud.pauget <arnaud.pauget@maarch.org>
2019-01-03 14:52:43 +0100

    Delete the additionnal Javascript file

--- web/public/js/modal/modalActions.js
+++
@@ -1,5 +0,0 @@
-$(".actionButton").on('click', function () {
-        var archiveIds = [$(this).data("archiveid")];
-        var actionId = $(this).data("actionid");
-        WorkflowAction.send(actionId, archiveIds);
-    });
