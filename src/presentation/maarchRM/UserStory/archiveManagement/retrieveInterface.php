1ef61bd80255a6fa3ee93513a5cb1dd579ba68a0
Cyril Vazquez <cyril.vazquez@maarch.org>
2019-03-15 12:16:00 +0100

    Move archiveInfo userStory command to app

--- src/presentation/maarchRM/UserStory/archiveManagement/retrieveInterface.php
+++ src/presentation/maarchRM/UserStory/archiveManagement/retrieveInterface.php
@@ -85,22 +85,6 @@ interface retrieveInterface
     );
 
     /**
-     * View the archive
-     *
-     * @return recordsManagement/archive/getDescription The recordsManagement/archive object
-     * @uses  recordsManagement/archiveDescription/read_archiveId_
-     */
-    public function readRecordsmanagementArchivedescription_archiveId_();
-
-    /**
-     * View the archive
-     *
-     * @return recordsManagement/archive/getArchiveDetails
-     * @uses  recordsManagement/archiveDescription/read_archiveId_
-     */
-    public function readRecordsmanagementArchiveinfo_archiveId_();
-
-    /**
      * Get metadata to edit
      *
      * @return recordsManagement/archive/getEditMetadata The recordsManagement/archive object
