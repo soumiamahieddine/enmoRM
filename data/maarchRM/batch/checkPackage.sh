a7d7d0976b272c87a3957278211f702d39e1fc07
arnaud.pauget <arnaud.pauget@maarch.org>
2019-03-27 17:22:00 +0100

    Upgrde shell script

--- data/maarchRM/batch/checkPackage.sh
+++ data/maarchRM/batch/checkPackage.sh
@@ -2,73 +2,96 @@
 
 libnames=(
 	"apache2"
-	"php"
-	"php-xml"
-	"php-pgsql"
-	"php-gd"
-	"php-mbstring"
-	"php-opcache"
 	"libapache2-mod-php"
 	"openssl"
 	"p7zip-full"
 	"default-jre"
 	"git")
 
+libnamesPHP=(
+	"xml"
+	"pgsql"
+	"gd"
+	"mbstring"
+	"opcache")
+	
 libnamesoptional=(
-	"php-mcrypt"
-	"php-pdo-pgsql")
+	"mcrypt"
+	"pdo-pgsql")
 
 moduleapache=(
 	"env_module"
 	"rewrite_module"
 	)
 
-echo "
------ REQUIRED ------
-"
+versionPHPmini=7.0
+
+echo -e "\n----- SYSTEM REQUIRED ------ \n"
 
 for i in "${libnames[@]}"
 do
 	dpkg -s $i &> /dev/null
-	dpkg=$?
+        dpkg=$?
 
-	command -v $i &> /dev/null
-	command=$?
-
-	if [ $dpkg -eq 0 ] || [ $command -eq 0 ]; then
-    	echo -e "Package \e[92m\e[1m$i\e[0m is installed!"
-	else
-	    echo -e "Package \e[91m\e[1m$i\e[0m is NOT installed!"
-	fi
+        if [ $dpkg -eq 0 ]; then
+                echo -e "Package \e[92m\e[1m${i}\e[0m is installed"
+        else
+                echo -e "Package \e[91m\e[1mphp-${i}\e[0m is NOT installed"
+        fi
 done
 
-echo "
------ OPTIONAL ------
-"
+phpVersion=$(php -v | head -n 1 | cut -d " " -f 2 | cut -f1-2 -d".")
 
-for i in "${libnamesoptional[@]}"
-do
-	dpkg -s $i &> /dev/null
-	dpkg=$?
+if (( $(echo "$phpVersion >= $versionPHPmini" |bc -l) ))
+then
+        echo -e "\nPHP VERSION : OK \n"
+	echo -e "\n----- PHP REQUIRED----- \n"
 
-	command -v $i &> /dev/null
-	command=$?
-	if [ $dpkg -eq 0 ] || [ $command -eq 0 ]; then
-    	echo -e "Package \e[92m\e[1m$i\e[0m is installed!"
-	else
-	    echo -e "Package \e[91m\e[1m$i\e[0m is NOT installed!"
-	fi
-done
+	for i in "${libnamesPHP[@]}"; do
+		
+		dpkg --list | cut -d ' ' -f3 | grep php | grep $phpVersion | grep $i &> /dev/null
+		dpkg=$?
 
-echo "
------ MODULE APACHE ------
-"
+		if [ $dpkg -eq 0 ]; then
+			package=$(dpkg --list | cut -d ' ' -f3 | grep php | grep $phpVersion | grep $i)
+			echo -e "Package \e[92m\e[1m${package}\e[0m is installed"
+		else
+			echo -e "Package \e[91m\e[1mphp-${i}\e[0m is NOT installed"
+		fi
+	done
+
+	echo -e "\n ----- PHP OPTIONAL ------ \n"
+
+	for i in "${libnamesoptional[@]}"
+	do
+		if [ $i = "mcrypt" ] && (( $(echo "$phpVersion >= 7.2" |bc -l) ))
+		then
+			continue
+		else
+			dpkg --list | cut -d ' ' -f3 | grep php | grep $phpVersion | grep $i &> /dev/null
+			dpkg=$?
+
+			if [ $dpkg -eq 0 ]; then
+				package=$(dpkg --list | cut -d ' ' -f3 | grep php | grep $phpVersion | grep $i)
+				echo -e "Package \e[92m\e[1m${package}\e[0m is installed"
+			else
+				echo -e "Package \e[91m\e[1mphp-${i}\e[0m is NOT installed"
+			fi
+		fi
+
+	done
+else
+	echo -e "\nPHP VERSION : KO \n"
+fi
+
+echo -e "\n----- MODULE APACHE ------ \n"
+
 for i in "${moduleapache[@]}"
 do
-	apache2ctl -M | grep $i &> /dev/null
+	apache2ctl -M 2> /dev/null | grep $i &> /dev/null
 	if [ $? -eq 0 ]; then
-    	echo -e "Package \e[92m\e[1m$i\e[0m is installed and activated !"
+    		echo -e "Package \e[92m\e[1m$i\e[0m is installed and activated !"
 	else
-	    echo -e "Package \e[91m\e[1m$i\e[0m is NOT installed or NOT activated !"
+		echo -e "Package \e[91m\e[1m$i\e[0m is NOT installed or NOT activated !"
 	fi
 done
