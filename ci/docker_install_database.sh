ecab0c7a102377030f8a916cb7c1c39323d9a854
Alexandre Morin <alexandre.morin@maarch.org>
2019-04-01 18:30:58 +0200

    Update docker_install_database.sh

--- ci/docker_install_database.sh
+++ ci/docker_install_database.sh
@@ -6,5 +6,5 @@ set -xe
 
 apt-get install postgresql-client -yqq
 
-psql -h "postgres" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -w < /builds/maarch/MaarchRM/data/maarchRM/batch/psql/structure.sql
-psql -h "postgres" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -w < /builds/maarch/MaarchRM/data/maarchRM/batch/psql/data_fr.sql
+psql -h "postgres" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -w < /builds/maarch/maarchRM/data/maarchRM/batch/psql/structure.sql
+psql -h "postgres" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -w < /builds/maarch/maarchRM/data/maarchRM/batch/psql/data_fr.sql
