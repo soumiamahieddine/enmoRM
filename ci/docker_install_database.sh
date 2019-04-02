01293c2140f63b2b2e20cc72673de7f4cb0f42c1
Alexandre Morin <alexandre.morin@maarch.org>
2019-04-02 09:38:57 +0200

    Update dpcker install database

--- ci/docker_install_database.sh
+++ ci/docker_install_database.sh
@@ -6,5 +6,5 @@ set -xe
 
 apt-get install postgresql-client -yqq
 
-psql -h "postgres" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -w < /builds/maarch/maarchRM/data/maarchRM/batch/psql/structure.sql
+psql -h "postgres" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -w < /builds/maarch/maarchRM/data/maarchRM/batch/psql/schema.sql
 psql -h "postgres" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -w < /builds/maarch/maarchRM/data/maarchRM/batch/psql/data_fr.sql
