592dfe05580d993c24be9e96dad58ac5f4ff5a71
Cyril Vazquez <cyril.vazquez@maarch.org>
2019-04-25 20:01:34 +0200

    Update file comments

--- src/bundle/recordsManagement/descriptionSchemeInterface.php
+++ src/bundle/recordsManagement/descriptionSchemeInterface.php
@@ -1,21 +1,21 @@
 <?php
 /*
- * Copyright (C) 2015 Maarch
+ * Copyright (C) 2019 Maarch
  *
- * This file is part of bundle organization.
+ * This file is part of bundle recordsManagement.
  *
- * Bundle organization is free software: you can redistribute it and/or modify
+ * Bundle recordsManagement is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Bundle organization is distributed in the hope that it will be useful,
+ * Bundle recordsManagement is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with bundle organization.  If not, see <http://www.gnu.org/licenses/>.
+ * along with bundle recordsManagement.  If not, see <http://www.gnu.org/licenses/>.
  */
 namespace bundle\recordsManagement;
 /**
