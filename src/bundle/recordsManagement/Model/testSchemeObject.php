be581e964db447f3f3fdea32f65c4184192ded6b
Cyril Vazquez <cyril.vazquez@maarch.org>
2019-04-29 12:24:10 +0200

    Add labels in sub object

--- src/bundle/recordsManagement/Model/testSchemeObject.php
+++ src/bundle/recordsManagement/Model/testSchemeObject.php
@@ -3,27 +3,32 @@ namespace bundle\recordsManagement\Model;
 class testSchemeObject
 {
     /**
+     * Chaîne
      * @var string
      */
     public $stringMetadata;
 
     /**
+     * Enum 
      * @var string
      * @enumeration [foo, bar]
      */
     public $enumMetadata;
 
     /**
+     * Date
      * @var date
      */
     public $dateMetadata;
 
     /**
+     * Nombre 
      * @var integer
      */
     public $numberMetadata;
 
     /**
+     * Indicateur
      * @var boolean
      */
     public $boolMetadata;
