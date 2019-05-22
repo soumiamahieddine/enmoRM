6c6a52a0cc0c8d3d802992a9771700bd38d10a60
Cyril Vazquez <cyril.vazquez@maarch.org>
2019-05-22 15:29:33 +0200

    Add SQL data for medona

---
+++ data/maarchRM/sql/pgsql/medona.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE "medona"."archivalAgreement" CASCADE;
+
+INSERT INTO "medona"."archivalAgreement" ("archivalAgreementId", "reference", "name", "description", "archiverOrgRegNumber", "depositorOrgRegNumber", "originatorOrgIds", "beginDate", "endDate", "allowedFormats", "enabled", "archivalProfileReference", "serviceLevelReference", "maxSizeTransfer", "maxSizeDay", "maxSizeWeek", "maxSizeMonth", "maxSizeYear", "maxSizeAgreement", "signed", "autoTransferAcceptance", "processSmallArchive") VALUES
+('MAARCH_SAS_invoice', 'MAARCH_SAS_invoice', 'Maarch SAS','invoice', 'org_123456789_Archives', 'org_45239273100025_Finance', 'MAARCH_SAS_finance', null, null, '', true, 'invoice', 'serviceLevel_001', null, null, null, null, null, 1000000, false, true, false),
+('MAARCH_WA_invoice', 'MAARCH_WA_invoice', 'Maarch WA','invoice', 'org_123456789_Archives', 'org_45239273100026_Finance', 'MAARCH_WA_finance', null, null, '', true, 'invoice', 'serviceLevel_002', null, null, null, null, null, 1000000, false, true, false);
