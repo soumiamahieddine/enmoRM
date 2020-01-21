cfa44f208a9e344910c531d5987a5c83a353355b
Jerome_maarch <jerome.boucher@maarch.org>
2020-01-21 14:20:41 +0100

    feat(#12584) : refactor to create a common default controller

---
+++ src/bundle/importExport/Controller/ImportExport.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * Copyright (C) 2020 Maarch
+ *
+ * This file is part of bundle organization.
+ *
+ * Bundle organization is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bundle organization is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with bundle organization.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace bundle\importExport\Controller;
+
+use core\Exception;
+
+/**
+ * Control of the organization
+ *
+ * @package importExport
+ */
+class ImportExport
+{
+    protected $sdoFactory;
+    protected $userAccountController;
+    protected $serviceAccountController;
+    protected $userPositionController;
+    protected $organizationController;
+    protected $archivalProfileController;
+    protected $roleController;
+    protected $descriptionFieldController;
+    protected $retentionRuleController;
+    protected $controller;
+    protected $model;
+    protected $limit;
+
+    /**
+     * Constructor
+     * @param \dependency\sdo\Factory $sdoFactory       The sdo factory
+     */
+    public function __construct(\dependency\sdo\Factory $sdoFactory, \dependency\localisation\TranslatorInterface $translator)
+    {
+        $this->sdoFactory = $sdoFactory;
+        $this->userAccountController = \laabs::newController('auth/userAccount');
+        $this->serviceAccountController = \laabs::newController('auth/serviceAccount');
+        $this->roleController = \laabs::newController('auth/role');
+        $this->organizationController = \laabs::newController('organization/organization');
+        $this->archivalProfileController = \laabs::newController('recordsManagement/archivalProfile');
+        $this->descriptionFieldController = \laabs::newController('recordsManagement/descriptionField');
+        $this->retentionRuleController = \laabs::newController('recordsManagement/retentionRule');
+        $this->controller = [
+            'userAccount' => $this->userAccountController,
+            'serviceAccount' => $this->serviceAccountController,
+            'role' => $this->roleController,
+            'organization' => $this->organizationController,
+            'archivalProfile' => $this->archivalProfileController,
+            'descriptionField' => $this->descriptionFieldController,
+            'retentionRule' => $this->retentionRuleController
+        ];
+
+        $this->model = [
+            'userAccount' => 'auth/account',
+            'serviceAccount' => 'auth/account',
+            'role' => 'auth/role',
+            'organization' => 'organization/organization',
+            'archivalProfile' => 'recordsManagement/archivalProfile',
+            'descriptionField' => 'recordsManagement/descriptionField',
+            'retentionRule' => 'recordsManagement/retentionRule'
+        ];
+
+        $this->limit =  \laabs::configuration('presentation.maarchRM')['maxResults'];
+    }
+}
