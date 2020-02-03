d40df8772a97d31c78ced6fed6035f5537a1dd6e
benjamin.rousseliere@maarch.org <benjamin.rousseliere@maarch.org>
2020-02-03 14:49:46 +0100

    feat/12584 : remove access of import/export in navbar if you don't have rights

--- src/presentation/maarchRM/UserStory/export/homeInterface.php
+++ src/presentation/maarchRM/UserStory/export/homeInterface.php
@@ -5,6 +5,7 @@ namespace presentation\maarchRM\UserStory\Export;
 interface homeInterface
 {
     /**
+     * @requires [export/archivalProfile, export/descriptionField, export/organization, export/retentionRule, export/serviceAccount, export/userAccount]
      *
      * @return importExport/Export/home
      */
--- src/presentation/maarchRM/UserStory/import/homeInterface.php
+++ src/presentation/maarchRM/UserStory/import/homeInterface.php
@@ -5,6 +5,7 @@ namespace presentation\maarchRM\UserStory\import;
 interface homeInterface
 {
     /**
+     * @requires [import/archivalProfile, import/descriptionField, import/organization, import/retentionRule, import/serviceAccount, import/userAccount]
      *
      * @return importExport/Import/home
      */
