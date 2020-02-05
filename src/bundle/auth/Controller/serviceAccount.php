ec4dcb5213415c016ca606cdd9c00f5af614fa01
Alexandre Morin <alexandre.morin@maarch.org>
2020-02-05 18:04:44 +0100

    fix () : minor bug

--- src/bundle/auth/Controller/serviceAccount.php
+++ src/bundle/auth/Controller/serviceAccount.php
@@ -322,7 +322,7 @@ class serviceAccount
                 throw new \core\Exception\UnauthorizedException("You are not allowed to do this action");
             }
         } elseif ($securityLevel == $account::SECLEVEL_FUNCADMIN) {
-            if (!$orgId || $serviceAccount->isAdmin) {
+            if ($serviceAccount->isAdmin && $serviceAccount->ownerOrgId != $account->ownerOrgId) {
                 throw new \core\Exception\UnauthorizedException("You are not allowed to do this action");
             }
         }
