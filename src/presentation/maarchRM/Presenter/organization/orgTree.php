15d93ccc4bdf0252824e240a21bc0577d3a8e658
Alexandre Morin <alexandre.morin@maarch.org>
2020-02-17 17:07:12 +0100

    fix () : display the tree if the configuration is missing

--- src/presentation/maarchRM/Presenter/organization/orgTree.php
+++ src/presentation/maarchRM/Presenter/organization/orgTree.php
@@ -65,7 +65,7 @@ class orgTree
     public function index($organizations, $orgType)
     {
         $orgRole = \laabs::configuration('organization')['orgUnitRoles'];
-        $isOrgTree = \laabs::configuration('organization')['isOrgTree'];
+        $isOrgTree = isset(\laabs::configuration('organization')['isOrgTree']) ? (bool) \laabs::configuration('organization')['isOrgTree'] : true;
 
         $this->view->addContentFile("organization/organizationIndex.html");
         $communicationMeans = \laabs::callService("contact/communicationMean/readIndex");
