0e66cb2f1738acb98287f3caac3aa4317ffdd691
benjamin.rousseliere@maarch.org <benjamin.rousseliere@maarch.org>
2020-03-06 16:42:19 +0100

    feat/13334 : update archive delivery

--- src/bundle/medona/Controller/ArchiveDeliveryRequest.php
+++ src/bundle/medona/Controller/ArchiveDeliveryRequest.php
@@ -227,7 +227,9 @@ class ArchiveDeliveryRequest extends abstractMessage
         $message->messageId = \laabs::newId();
 
         $schema = "mades";
-        if (\laabs::hasBundle('seda')) {
+        if ($archives[0]->descriptionClass === 'seda2') {
+            $schema = "seda2";
+        } elseif (\laabs::hasBundle('seda')) {
             $schema = "seda";
         }
         $message->schema = $schema;
