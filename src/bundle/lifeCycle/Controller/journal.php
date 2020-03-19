a37f2161cadb96a64ed52641fd9f7abf285b6458
Alexandre Morin <alexandre.morin@maarch.org>
2020-03-19 17:38:46 +0100

    feat (11784) : fix check integrity

--- src/bundle/lifeCycle/Controller/journal.php
+++ src/bundle/lifeCycle/Controller/journal.php
@@ -979,10 +979,8 @@ class journal
 
         $resources = $archiveController->getDigitalResources($nextJournal->archiveId, $checkAccess = false);
         $nextJournalResource = $digitalResourceController->retrieve($resources[0]->resId);
-        $nextJournalContents = $nextJournalResource->getContents();
-
-        $chainEvent = str_getcsv(strtok($nextJournalContents, "\n"));
-
+        $nextJournalContents = $nextJournalResource->getHandler();
+        $chainEvent = fgetcsv($nextJournalContents);
         // For older version compatibility
         if (count($chainEvent) < 7) {
             if (empty($chainEvent[3]) || empty($chainEvent[4]) || empty($chainEvent[5])) {
@@ -1003,9 +1001,11 @@ class journal
             $chainedJournalHashAlgo = $chainEvent[4];
             $chainedJournalHash = $chainEvent[5];
 
-            $calcJournalHash = hash($chainedJournalHashAlgo, $journalResource->getContents());
+            $journalFilename = $this->copyJournalIntoCsv($journalResource);
+            $calcJournalHash = hash_file($chainedJournalHashAlgo, $journalFilename);
 
             if ($calcJournalHash != $chainedJournalHash) {
+                unlink($journalFilename);
                 throw \laabs::newException(
                     'recordsManagement/journalException',
                     "Invalid journal: Chaining event has a different hash."
@@ -1031,9 +1031,11 @@ class journal
             $chainedJournalHashAlgo = $chainEvent[9];
             $chainedJournalHash = $chainEvent[10];
 
-            $calcJournalHash = hash($chainedJournalHashAlgo, $journalResource->getContents());
+            $journalFilename = $this->copyJournalIntoCsv($journalResource);
+            $calcJournalHash = hash_file($chainedJournalHashAlgo, $journalFilename);
 
             if ($calcJournalHash != $chainedJournalHash) {
+                unlink($journalFilename);
                 throw \laabs::newException(
                     'recordsManagement/journalException',
                     "Invalid journal: Chaining event has a different hash."
@@ -1041,6 +1043,7 @@ class journal
             }
         }
 
+        unlink($journalFilename);
         return $chainEvent;
     }
 
