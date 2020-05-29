f04ef695e7a0d03852f16c8d6c76eb74577a3b00
Jerome_maarch <jerome.boucher@maarch.org>
2020-05-29 17:22:57 +0200

    feat(#12467) : rework function html and add comments

--- src/bundle/Statistics/Controller/Statistics.php
+++ src/bundle/Statistics/Controller/Statistics.php
@@ -609,6 +609,14 @@ EOT;
         return $sum;
     }
 
+    /**
+     * Retrieve size of digital resources unto a specific date group by a parameter
+     *
+     * @param  string   $groupBy Ordering parameter
+     * @param  datetime $endDate End date
+     *
+     * @return array             Size of archive ordered by parameter
+     */
     protected function getArchiveSizeOrdered($groupBy, $endDate = null)
     {
         switch ($groupBy) {
@@ -648,6 +656,14 @@ EOT;
         return $results;
     }
 
+    /**
+     * Retrieve count of digital resources unto a specific date group by a parameter
+     *
+     * @param  string   $groupBy Ordering parameter
+     * @param  datetime $endDate End date
+     *
+     * @return array             Count of archive ordered by parameter
+     */
     protected function getArchiveCountOrdered($groupBy, $endDate = null)
     {
         switch ($groupBy) {
