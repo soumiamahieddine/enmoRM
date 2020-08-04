79622dfdaa16ada06d985dfdcdccd82779655e20
maarch <benjamin.rousseliere@maarch.org>
2020-08-04 16:22:33 +0200

    feat/12467 : add documentation

--- src/bundle/Statistics/Controller/Statistics.php
+++ src/bundle/Statistics/Controller/Statistics.php
@@ -52,7 +52,7 @@ class Statistics
     /**
      * Retrieve default stats for screen
      *
-     * @return array $statistics
+     * @return array Associative array of properties
      */
     public function index()
     {
@@ -60,13 +60,13 @@ class Statistics
     }
 
     /**
-     * Get Counts
+     * Retrieve filtered stats
      *
      * @param  string $operation  Type of operation to count or sum
      * @param  string $startDate  Start date
      * @param  string $endDate    End date
      * @param  string $filter     Filtering parameter to group query by
-     * @param  float  $sizeFilter power of 10 to divide by
+     * @param  float  $sizeFilter Power of 10 to divide by
      *
      * @return array              Associative array of properties
      */
@@ -77,8 +77,7 @@ class Statistics
         } else {
             $startDate = null;
         }
-
-        if (!empty($startDate)) {
+        if (!empty($endDate)) {
             $endDate = \laabs::newDateTime($endDate);
         } else {
             $endDate = null;
@@ -103,13 +102,13 @@ class Statistics
 
         $this->sizeFilter = $sizeFilter;
 
-        $statistics = ["unit" => $this->sizeFilters[$sizeFilter]];
+        $statistics = ["unit" => $this->sizeFilters[$this->sizeFilter]];
         if (is_null($operation) || empty($operation)) {
             $statistics = $this->defaultStats($startDate, $endDate, $statistics);
         } elseif (!empty($operation) && !in_array($operation, ['deposit', 'deleted', 'conserved', 'restituted', 'transfered', 'communicated'])) {
             throw new \core\Exception\BadRequestException($this->translator->getText("Operation type not supported"));
         } elseif (!empty($operation) && is_null($filter)) {
-            throw new \core\Exception\BadRequestException($this->translator->getText("Filter cannot be null if operation is not"));
+            throw new \core\Exception\BadRequestException($this->translator->getText("Filter cannot be null if an operation is selected"));
         }
 
         if (!empty($operation)) {
@@ -216,6 +215,7 @@ class Statistics
      * @param  datetime $startDate starting date
      * @param  datetime $endDate   End date
      * @param  string   $filter    Group by argument
+     * @param  array    $statistics Array of statistics
      *
      * @return array               Associative of statistics
      */
@@ -238,6 +238,7 @@ class Statistics
      * @param  datetime $startDate starting date
      * @param  datetime $endDate   End date
      * @param  string   $filter    Group by argument
+     * @param  array    $statistics Array of statistics
      *
      * @return array               Associative of statistics
      */
@@ -259,6 +260,7 @@ class Statistics
      *
      * @param  datetime $endDate   End date
      * @param  string   $filter    Group by argument
+     * @param  array    $statistics Array of statistics
      *
      * @return array               Associative of statistics
      */
@@ -276,6 +278,7 @@ class Statistics
      * @param  datetime $startDate starting date
      * @param  datetime $endDate   End date
      * @param  string   $filter    Group by argument
+     * @param  array    $statistics Array of statistics
      *
      * @return array               Associative of statistics
      */
@@ -294,6 +297,7 @@ class Statistics
      * @param  datetime $startDate starting date
      * @param  datetime $endDate   End date
      * @param  string   $filter    Group by argument
+     * @param  array    $statistics Array of statistics
      *
      * @return array               Associative of statistics
      */
@@ -312,6 +316,7 @@ class Statistics
      * @param  datetime $startDate starting date
      * @param  datetime $endDate   End date
      * @param  string   $filter    Group by argument
+     * @param  array    $statistics Array of statistics
      *
      * @return array               Associative of statistics
      */
@@ -327,10 +332,12 @@ class Statistics
     /**
      * Sum all event info for a particular event
      *
+     * @param  array    $messageType      The type of the message
      * @param  array    $eventTypes       Array of event types
      * @param  integer  $jsonColumnNumber json Column number for size parameter in lifeCycle event table
      * @param  datetime $startDate        Starting Date
      * @param  datetime $endDate          End date
+     * @param  boolean  $isIncoming       Is the message incoming if type is Archive Transfer
      *
      * @return integer                    Sum of size for events
      */
@@ -640,13 +647,7 @@ EOT;
         ).
         ' GROUP BY '.($isArchivalProfile ? '"archivalProfile"."name"' : '"organization"."displayName"');
         
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute();
-        $results = [];
-        while ($result = $stmt->fetch(\PDO::FETCH_ASSOC)) {
-            $result['sum'] = isset($result['sum']) ? $this->formatNumber($result['sum']) : '0.000';
-            $results[] = $result;
-        }
+        $results = $this->executeQuery($query);
 
         return $results;
     }
@@ -686,12 +687,7 @@ EOT;
                 ' WHERE "depositDate" < \''.$endDate.'\'::timestamp AND ("status" = \'preserved\' OR ("lastModificationDate" IS NOT NULL AND "lastModificationDate">\''.$endDate.'\'::timestamp)) AND "archive"."parentArchiveId" IS NULL
                 GROUP BY '.($isArchivalProfile ? '"archivalProfile"."name"' : '"organization"."displayName"');
 
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute();
-        $results = [];
-        while ($result = $stmt->fetch(\PDO::FETCH_ASSOC)) {
-            $results[] = $result;
-        }
+        $results = $this->executeQuery($query);
 
         return $results;
     }
@@ -707,16 +703,16 @@ EOT;
     protected function getSizeForDirectEvent($eventType, $jsonSizeColumnNumber, $groupBy = null, $startDate = null, $endDate = null)
     {
         if ($groupBy) {
-            $selectCondition = $groupBy == 'archivalProfile' ? 'COALESCE("archivalProfile"."name", \'Without profile\')' : '"organization"."displayName"';
-            $groupByCondition = $groupBy == 'archivalProfile' ? '"archivalProfile"."name"' : '"organization"."displayName"';
-            $joinCondition = $groupBy == 'archivalProfile'
+            $selectCondition = ($groupBy == 'archivalProfile') ? 'COALESCE("archivalProfile"."name", \'Without profile\')' : '"organization"."displayName"';
+            $groupByCondition = ($groupBy == 'archivalProfile') ? '"archivalProfile"."name"' : '"organization"."displayName"';
+            $joinCondition = ($groupBy == 'archivalProfile')
                 ? ' LEFT JOIN "recordsManagement"."archivalProfile" "archivalProfile"
                 ON "archivalProfile"."reference" = "event"."eventInfo"::json->>10'
                 : ' INNER JOIN "organization"."organization" "organization"
                 ON "organization"."registrationNumber" = "event"."eventInfo"::json->>6';
         }
 
-        $query = 'SELECT '.($groupBy ? $selectCondition . ' AS '.$groupBy.', ' : '').'SUM(CAST("event"."eventInfo"::json->>'.$jsonSizeColumnNumber.' AS INTEGER))
+        $query = 'SELECT '.($groupBy ? $selectCondition . ' AS "'.$groupBy.'", ' : '').'SUM(CAST("event"."eventInfo"::json->>'.$jsonSizeColumnNumber.' AS INTEGER))
         FROM "lifeCycle"."event" "event"'.
         ($groupBy ? $joinCondition : '').'
         WHERE "event"."eventType" IN (\''.$eventType.'\')
@@ -731,7 +727,6 @@ EOT;
 
         $sum = 0;
         if ($groupBy) {
-            $groupBy = strtolower($groupBy);
             $sum = [];
             foreach ($result as $row) {
                 if (isset($row[$groupBy])) {
@@ -765,7 +760,7 @@ EOT;
                 ON "organization"."registrationNumber" = "event"."eventInfo"::json->>6';
         }
 
-        $query = 'SELECT '.($groupBy ? $selectCondition . ' AS '.$groupBy.', ' : '').'COUNT("event"."eventId")
+        $query = 'SELECT '.($groupBy ? $selectCondition . ' AS "'.$groupBy.'", ' : '').'COUNT("event"."eventId")
         FROM "lifeCycle"."event" "event"'.
         ($groupBy ? $joinCondition : '').'
         WHERE "event"."eventType" IN (\''.$eventType.'\')
@@ -780,7 +775,6 @@ EOT;
         
         $count = 0;
         if ($groupBy) {
-            $groupBy = strtolower($groupBy);
             $count = [];
             foreach ($result as $row) {
                 if (isset($row[$groupBy])) {
