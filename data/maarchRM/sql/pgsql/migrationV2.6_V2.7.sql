475eafc98525441f175b95538f5589ebf208c07b
Cyril Vazquez <cyril.vazquez@maarch.org>
2020-10-09 11:54:44 +0200

    Update migrationV2.6_V2.7.sql

--- data/maarchRM/sql/pgsql/migrationV2.6_V2.7.sql
+++ data/maarchRM/sql/pgsql/migrationV2.6_V2.7.sql
@@ -1,6 +1,6 @@
 -- Migration script for PGSQL from Maarch RM V2.6 to V2.7
 
 UPDATE "lifeCycle"."eventFormat" SET format = 'type senderOrgRegNumber senderOrgName recipientOrgRegNumber recipientOrgName reference code info' WHERE type = 'medona/sending';
-INSERT INTO "lifeCycle"."eventFormat" (type, format, message, notification) VALUES ('digitalResource/integrityCheck', 'repositoryReference addressesToCheck checkedAddresses failed', 'Contrôle d''intégrité des ressources présentes dans %6$s', false)
+INSERT INTO "lifeCycle"."eventFormat" (type, format, message, notification) VALUES ('digitalResource/integrityCheck', 'repositoryReference addressesToCheck checkedAddresses failed', 'Contrôle d''intégrité des ressources présentes dans %6$s', false);
 
 ALTER TABLE "organization"."organization" ADD COLUMN "history" text;
