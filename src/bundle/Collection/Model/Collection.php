bb8b805e97c28efaec6a16103f8b201dccd5c413
Jerome_maarch <jerome.boucher@maarch.org>
2021-03-01 12:07:53 +0100

    feat/16378 : init crud

---
+++ src/bundle/Collection/Model/Collection.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * Copyright (C) 2021 Maarch
+ *
+ * This file is part of bundle collection.
+ *
+ * Bundle collection is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bundle collection is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with bundle collection.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace bundle\Collection\Model;
+
+/**
+ * collection definition
+ *
+ * @package Collection
+ *
+ * @pkey [collectionId]
+ *
+ */
+class Collection
+{
+
+    /**
+     * The collection Ientifier
+     *
+     * @var id
+     * @notempty
+     */
+    public $collectionId;
+
+    /**
+     * The account identifier
+     *
+     * @var string
+     *
+     */
+    public $name;
+
+    /**
+     * Array of archive Ids
+     *
+     * @var json
+     *
+     */
+    public $archiveIds;
+}
---
+++ src/bundle/Collection/UserCollectionInterface.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Copyright (C) 2021 Maarch
+ *
+ * This file is part of bundle collection.
+ *
+ * Bundle collection is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bundle collection is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with bundle collection.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace bundle\Collection;
+
+/**
+ * Interface for UserCollections
+ *
+ * @package Collection
+ * @author  Jérôme Boucher <jerome.boucher@maarch.org>
+ */
+interface UserCollectionInterface
+{
+
+}
