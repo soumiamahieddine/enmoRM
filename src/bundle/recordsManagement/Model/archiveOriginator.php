39a5466f65f3d88b66f3c4d4af5f4f35a7ec9257
Charlotte Bataille <charlotte.bataille@xelians.fr>
2021-11-04 10:52:11 +0100

    feat/18079 : fix route

---
+++ src/bundle/recordsManagement/Model/archiveOriginator.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * Copyright (C) 2015 Maarch
+ *
+ * This file is part of bundle recordsManagement.
+ *
+ * Bundle recordsManagement is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bundle recordsManagement is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with bundle recordsManagement.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace bundle\recordsManagement\Model;
+/**
+ * Class model that represents an archive
+ *
+ * @package RecordsManagement
+ * @author  Cyril VAZQUEZ (Maarch) <cyril.vazquez@maarch.org>
+ * 
+ * @substitution recordsManagement/archive
+ * 
+ * @pkey [archiveId]
+ */
+class archiveStatus
+{
+    /**
+     * The archive identifier
+     *
+     * @var string
+     * @notempty
+     */
+    public $archiveId;
+
+    /**
+     * The status
+     *
+     * @var string
+     * @enumeration [pending, preserved, frozen, disposable, disposed]
+     */
+    public $status;
+
+    /**
+     * @var timestamp
+     */
+    public $lastModificationDate;
+
+
+} // END class archive 
